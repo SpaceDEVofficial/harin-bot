56a3609d9e05fef158a62f06ecfdd666c86054a9
MisileLab <misileminecord@gmail.com>
2021-10-11 14:39:45 +0900

    refactor: delete unused import and other refactor

--- cogs/command_hinter.py
+++ cogs/command_hinter.py
@@ -1,13 +1,9 @@
-import io
-import asyncio
-import urllib.request
-import aiosqlite
-import discord
-from PIL import Image
-from discord.ext import commands
-import discordSuperUtils
 import random
 from typing import List
+
+import discordSuperUtils
+from discord.ext import commands
+
 
 def random_chat(suggest):
     chatlist = ["혹시 `하린아 {hint}`를 사용하시려고 한건가요?",
@@ -16,14 +12,14 @@ def random_chat(suggest):
     res = random.choice(chatlist)
     return res.format(hint=suggest)
 
+
 class MyCommandGenerator(discordSuperUtils.CommandResponseGenerator):
     def generate(self, invalid_command: str, suggestion: List[str]) -> str:
         # This is only an example, you can use the default generator if you want to.
-        res = random_chat(suggest=suggestion[0])
-        return res
+        return random_chat(suggest=suggestion[0])
 
 
-class commandhint(commands.Cog):
+class CommandHint(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.ImageManager = discordSuperUtils.ImageManager()
@@ -31,4 +27,4 @@ class commandhint(commands.Cog):
 
 
 def setup(bot):
-    bot.add_cog(commandhint(bot))
+    bot.add_cog(CommandHint(bot))
